6ce952e68d16a9c72423b8464f27133dfd50549f
Linnovs Liu <no-reply@linnovs.com>
2025-11-21 02:14:33 +0800

    feat(middleware): add logging

---
+++ middlewares.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/charmbracelet/log"
+)
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func (lrw *loggingResponseWriter) Flush() {
+	if flusher, ok := lrw.ResponseWriter.(http.Flusher); ok {
+		flusher.Flush()
+	}
+}
+
+func newLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func loggingMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		w = newLoggingResponseWriter(w)
+		logger := log.With(
+			"remote_addr", r.RemoteAddr,
+			"host", r.Host,
+			"method", r.Method,
+			"uri", r.RequestURI,
+			"user_agent", r.UserAgent(),
+		)
+		r = r.WithContext(context.WithValue(r.Context(), log.ContextKey, logger))
+
+		handler.ServeHTTP(w, r)
+		logger.Info(
+			"request completed",
+			"status", w.(*loggingResponseWriter).statusCode,
+			"resspone_time", time.Since(start).String(),
+		)
+	})
+}
